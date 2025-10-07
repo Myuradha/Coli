471db6db00ab98a85fc4dbcf05d9668d2feb33e5
Myuradha <rdhany050@gmail.com>
2025-10-07 17:39:21 +0700

    pt3_update3.1

--- c/pt_3/konversi_jarak_x.c
+++ c/pt_3/konversi_jarak_x.c
@@ -10,9 +10,8 @@ int main() {
 	km = x / 100000;
 	sisa = x % 100000;
 	m = sisa / 100;
-	cm = sisa % 1000;
+	cm = sisa % 100;
 	
 	printf("%i\n", sisa);
 	printf("%ikm + %im + %icm", km, m, cm);
-	
 }
--- c/pt_3/membaca_nilai_uang.c
+++ c/pt_3/membaca_nilai_uang.c
@@ -31,3 +31,38 @@ int main() {
     
     return 0;
 }
+
+/*Program membaca_nilai_uang
+	{Program ini akan membaca nilai uang menjadi pecahan Rp1000, Rp500, Rp100, Rp50, Rp25}
+
+Kamus
+	lembar1000, lembar500, lembar100, lembar50, lembar25 : interger
+
+Algoritma
+	{masukkan jumlah uang}
+	output("Masukkan jumlah uang: ")
+	input(uang)
+	
+	{menghitung pecahan uang}
+	lembar1000 = uang div 1000
+	uang = uang mod 1000
+	
+	lembar500 = uang div 500
+	uang = uang mod 500
+	
+	lembar100 = uang div 100
+	uang = uang mod 100
+	
+	lembar50 = uang div 50
+	uang = uang mod 50
+	
+	lembar25 = uang div 25
+	uang = uang mod 25
+	
+	{tampilkan pecahan uang}
+	output("lembar1000 = ", lembar1000)
+	output("lembar500 = ", lembar500)
+	output("lembar100 = ", lembar100)
+	output("lembar50 = ", lembar50)
+	output("lembar25 = ", lembar25)
+*/
